eeb46efbc5735c18c2d1cf0a7fd549607f717bb4
Matthew Killeya <matthew.killeya@cantabcapital.com>
2016-10-14 17:51:56 +0100

    First version

--- example.py
+++ example.py
@@ -14,7 +14,7 @@ from collections import defaultdict
 from PyQt5.QtWidgets import QApplication
 
 from maze import Maze, Game, game_repeater
-from goodies import RandomGoody
+from goodies import RandomGoody,MattGoody
 from baddies import RandomBaddy
 from gui import GameViewer
 
@@ -33,7 +33,8 @@ EXAMPLE_MAZE = Maze(10, 10, "0001010000"
 def text_example():
     ''' Prints the state of the game to stdout after each round of turns '''
 
-    goody0 = RandomGoody()
+    goody0 = MattGoody()
+    #goody0 = RandomGoody()
     goody1 = RandomGoody()
     baddy = RandomBaddy()
 
@@ -49,7 +50,10 @@ def stats_example(total_games):
     ''' Plays many games, printing cumulative and final stats '''
 
     results = defaultdict(int)
-    for game_number, game in enumerate(game_repeater(EXAMPLE_MAZE, RandomGoody, RandomGoody, RandomBaddy)):
+    #for game_number, game in enumerate(game_repeater(EXAMPLE_MAZE, RandomGoody, RandomGoody, RandomBaddy)):
+    for game_number, game in enumerate(game_repeater(EXAMPLE_MAZE, MattGoody, RandomGoody, RandomBaddy)):
+    #for game_number, game in enumerate(game_repeater(EXAMPLE_MAZE, MattGoody, MattGoody, RandomBaddy)):
+
         if game_number == total_games:
             break
         result, _rounds = game.play()
@@ -69,6 +73,6 @@ def gui_example():
 
 if __name__ == "__main__":
     # Uncomment whichever example you want to run
-    text_example()
-    # stats_example(1000)
-    # gui_example()
+    #text_example()
+    stats_example(1000)
+    #gui_example()
--- goodies.py
+++ goodies.py
@@ -6,7 +6,7 @@
 
 import random
 
-from maze import Goody, UP, DOWN, LEFT, RIGHT, STAY, PING
+from maze import Goody, UP, DOWN, LEFT, RIGHT, STAY, PING, ZERO, Position
 
 class StaticGoody(Goody):
     ''' A static goody - does not move from its initial position '''
@@ -22,3 +22,68 @@ class RandomGoody(Goody):
         ''' Ignore any ping information, just choose a random direction to walk in, or ping '''
         possibilities = filter(lambda direction: not obstruction[direction], [UP, DOWN, LEFT, RIGHT]) + [PING]
         return random.choice(possibilities)
+
+def move_to_location(move):
+    ''' This maps a 'move' label to an x and y increment. '''
+    mapper = {
+        STAY  : ZERO,
+        LEFT  : Position(-1, 0),
+        RIGHT : Position(1, 0),
+        UP    : Position(0, 1),
+        DOWN  : Position(0, -1)}
+    return mapper[move]
+
+def distance(position1, position2):
+    ''' Find distance between two points '''
+    return (abs(position1.x - position2.x) +  abs(position1.y - position2.y))
+
+class MattGoody(Goody):
+    ''' Matt is a goody '''
+
+
+
+    def __init__(self):
+        super(MattGoody, self).__init__()
+
+        self.location = ZERO
+        self.previous_locations = [ZERO]
+        self.ping = None
+
+    def take_turn(self, obstruction, _ping_response):
+        ''' Ping first time. After that try and minimize distance to ping '''
+
+        # Makes sense to store location and pings as absolutes, as there can be a lot moves in between pings .
+
+        # We ping first time if no one has #
+        if self.ping is None and _ping_response is None:
+            return PING
+        elif _ping_response is not None:  # otherwise update if there is a new ping #
+            self.ping  = dict((key, value + self.location)
+                            for key, value in _ping_response.iteritems())
+
+        # We don't know how ping_response will be keyed as not sure what type of goody.
+        goody_key = [key for key in self.ping.keys() if isinstance(key, Goody)][0]
+        goody_ping = self.ping[goody_key]
+
+        # We are the target location (location of last ping). Need to ping - they must have moved.
+        if distance(goody_ping, self.location) < 1:
+            return PING
+
+        possibilities = filter(lambda direction: not obstruction[direction], [UP, DOWN, LEFT, RIGHT])
+
+        # Calc distance to goody of all possibilities
+        distances = [distance(self.location + move_to_location(possibility), goody_ping)
+                      for possibility in possibilities]
+        # Return moves which minimizes distance to last known location of goody
+        move = possibilities[distances.index(min(distances))]
+        move_to_loc = move_to_location(move)
+
+        # If been here before, do random move instead #
+        if self.location + move_to_loc in self.previous_locations:
+            move = random.choice(possibilities)
+            move_to_loc = move_to_location(move)
+
+        self.location = self.location + move_to_loc
+        self.previous_locations.append(self.location)
+
+        return move
